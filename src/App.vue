b61a2d8a33abaad6c694031aac5d0a3bc6646110
Matheo Cabrera <mcabre24@stu.southtexascollege.edu>
2024-09-22 12:31:11 -0500

    Completed total balance component. Modified to include subtotal and tax amounts and calculate them accurately.

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,85 @@
 <script setup>
   import Header from './components/Header.vue';
+  import TotalBalance from './components/TotalBalance.vue';
+  import ItemsDiscounts from './components/ItemsDiscounts.vue';
+  import AddGroceryItem from './components/AddGroceryItem.vue';
+  import GroceryList from './components/GroceryList.vue';
+
+  import {ref, computed, onMounted} from 'vue'
+
+  const transactions = ref([
+  ])
+
+  const calcSub = computed(()=>{
+    return transactions.value.reduce((acc, x)=>{
+      return acc+x.amount
+    },0)
+  })
+
+  const calcTax = computed(()=>{
+    return transactions.value.reduce((acc, x)=>{
+      return (acc+x.amount*0.0825).toFixed(3)
+    },0)
+  })
+
+  const calcTot = computed(()=>{
+    return transactions.value.reduce((acc, x)=>{
+      return (acc+x.amount*1.0825).toFixed(2)
+    },0)
+  })
+
+  const moneyIn = computed(()=>{
+    return transactions.value
+    .filter((x)=>x.amount>0)
+    .reduce((acc, x)=>{
+      return acc+x.amount
+    },0)
+  })
+  const moneyOut = computed(()=>{
+    return transactions.value
+    .filter((x)=>x.amount<0)
+    .reduce((acc, x)=>{
+      return acc+x.amount
+    },0)
+  })
+
+  const handleTransaction = (transactionData) => {
+    transactions.value.push({
+      id: generateID(),
+      text: transactionData.text,
+      amount: transactionData.amount,
+    })
+    saveToLocalStorage()
+  }
+
+  const generateID = () =>{
+    return Math.floor(Math.random()*10000000)
+  }
+
+  const handleDelete = (id) =>  {
+    transactions.value = transactions.value.filter((x) => x.id !== id)
+    saveToLocalStorage()
+  }
+
+  const saveToLocalStorage = () => {
+    localStorage.setItem('transactions', JSON.stringify(transactions.value))
+  }
+
+  onMounted(() => {
+    const savedTransactions = JSON.parse(localStorage.getItem('transactions'))
+
+    if(savedTransactions){
+      transactions.value = savedTransactions
+    }
+  })
 </script>
 
 <template>
-  <Header></Header>
+    <Header></Header>
+    <div class="container">
+      <TotalBalance :subTotal="calcSub" :tax="calcTax" :total="calcTot"></TotalBalance>
+      <ItemsDiscounts :income="moneyIn" :expense="moneyOut"></ItemsDiscounts>
+      <AddGroceryItem @transactionSubmitted="handleTransaction"></AddGroceryItem>
+      <GroceryList :transactions="transactions" @transactionDeleted="handleDelete"></GroceryList>
+    </div>
 </template>
-
---
+++ src/components/TotalBalance.vue
@@ -0,0 +1,26 @@
+<script setup>
+    import { defineProps } from 'vue';
+
+    const props = defineProps({
+        subTotal: {
+            type: Number,
+            require: true,   
+        },
+        tax: {
+            type: Number,
+            require: true,   
+        },
+        total: {
+            type: Number,
+            require: true,     
+        }
+    })
+</script>
+<template>
+    <h4>SubTotal</h4>
+    <h1 id="balance">{{ subTotal }}</h1>
+    <h4>Tax</h4>
+    <h1 id="balance">{{ tax }}</h1>
+    <h4>Total</h4>
+    <h1 id="balance">{{ total }}</h1>
+</template>
